58e193b44d1fadfa59e9fcf00436e41c4e68b271
JesusMtnez <jesusmartinez93@gmail.com>
2017-07-24 14:16:02 +0200

    Add 'Hello World' problem

---
+++ src/main/scala/org/jesusmtnez/hackerank/fp/HelloWorld.scala
@@ -0,0 +1,13 @@
+package org.jesusmtnez.hackerank.fp
+
+/**
+  * A "Hello World" program has been widely adopted as the introductory program for learning how to code.
+  * So, let's start functional programming by writing the same program.
+  */
+object HelloWorld extends App {
+  def f() = {
+    println("Hello World")
+  }
+
+  f
+}
